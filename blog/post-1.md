3ed5db01d52a9da7c48e6319bd0b0d9d70fad7fb
BrunoElo <elochux@gmail.com>
2022-05-02 03:14:43 +0100

    feat: implemented all blog posts page...WIP

---
+++ blog/post-1.md
@@ -0,0 +1,12 @@
+---
+title: Post 1
+description: blog description
+published: true
+image: /assets/images/enigma.jpg
+seo: {
+metaDescription: First post for my angular scully blog,
+metaTitle: First post
+}
+---
+
+# Post 1
--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ScullyRoute, ScullyRoutesService} from "@scullyio/ng-lib";
+import {Observable} from "rxjs";
+import { map, tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-blog-list',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-list.component.scss']
 })
 export class BlogListComponent implements OnInit {
+  links$: Observable<ScullyRoute[]> = this.scully.available$;
 
-  constructor() { }
+  constructor(private scully: ScullyRoutesService) { }
 
   ngOnInit(): void {
+    this.getPublishedPosts();
   }
 
+  getPublishedPosts() {
+    this.links$ = this.links$.pipe(map((links) => links.filter((link) =>
+      link.route.startsWith('/blog/'))),tap((val) => console.log(val)));
+  }
 }
